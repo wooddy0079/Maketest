17524178bf77e073310bba44b6468af4c639278d
addischang1991 <addischang1991@livemail.tw>
2015-02-22 12:52:42 +0800

    The first edtion for automake

---
+++ Test2/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+// The main loop
+int main()
+{
+	// Declare function areacalculator as float 
+	float areacalculator( float );
+
+	// Declare variable a, b, c as float 
+ 	float a, b, c, r, area;
+	r = 15.5;
+	a = 12.4;
+	b = 18.5;
+	c = a + b;
+
+
+	// Print the summation of a & b.
+	printf( " %f \n ", c );
+
+	// Print the script " Hello World ! "
+	printf( " Hello World ! \n " );
+
+	//
+	return 0;
+}
+
